849c565c7660669a89e4cbecb2c15ee410159b00
Kush Patel <kushrpatel06@yahoo.com>
2022-12-06 15:25:12 -0500

    Models for db finished making the models for each table and their columns for the db

---
+++ models/User.js
@@ -0,0 +1,62 @@
+//imports
+const {Model, DataTypes} = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
+//User model
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+//defining columns and their properties/alowed values
+User.init(
+    {
+        id :{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8]
+            }
+        }
+    },
+    {
+        hooks: {//password hashing
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+                
+            },
+            // set up beforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "user"
+      }
+);
+
+module.exports = User;
